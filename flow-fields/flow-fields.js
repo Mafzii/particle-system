ee298c0ebda98d382b2d169818709d47a7c8d50a
Mafzii <23100040@lums.edu.pk>
2024-01-11 22:58:22 +0500

    Functional flow field with media controls

--- flow-fields/flow-fields.js
+++ flow-fields/flow-fields.js
@@ -21,7 +21,11 @@ document.addEventListener("DOMContentLoaded", function () {
         x: Math.random() * 5 - 2.5,
         y: Math.random() * 2 - 1,
       };
+      this.speedModifier = Math.round(Math.random() * 2 + 1);
       this.history = [{ x: this.x, y: this.y }];
+      this.maxLength = Math.floor(Math.random() * 100 + 50);
+      this.angle = 0;
+      this.timer = this.maxLength * 2;
     }
     draw(context) {
       context.fillRect(this.x, this.y, 1, 1);
@@ -33,9 +37,34 @@ document.addEventListener("DOMContentLoaded", function () {
       context.stroke();
     }
     update() {
-      this.x += this.velocity.x + Math.random() *5 -2.5;
-      this.y += this.velocity.y;
+      // When timer is up slowly remove the line by removing its history
+      this.timer--;
+      if (this.timer < 0) {
+        if (this.history.length > 1) this.history.shift(); // Keep origin point
+        else this.reset(); // Reset particle animation when removed all but origin
+        return;
+      }
+      // Get the angle from the flow field
+      const col = Math.floor(this.x / this.effect.cellSize);
+      const row = Math.floor(this.y / this.effect.cellSize);
+      const index = col + row * this.effect.cols;
+      this.angle = this.effect.flowField[index];
+
+      // Update the velocity
+      this.velocity.x = Math.cos(this.angle);
+      this.velocity.y = Math.sin(this.angle);
+      this.x += this.velocity.x * this.speedModifier;
+      this.y += this.velocity.y * this.speedModifier;
+
       this.history.push({ x: this.x, y: this.y });
+      // Keep the line at a certain length
+      if (this.history.length > this.maxLength) this.history.shift();
+    }
+    reset() {
+      this.x = Math.floor(Math.random() * this.effect.width);
+      this.y = Math.floor(Math.random() * this.effect.height);
+      this.history = [{ x: this.x, y: this.y }];
+      this.timer = this.maxLength * 2;
     }
   }
 
@@ -44,10 +73,29 @@ document.addEventListener("DOMContentLoaded", function () {
       this.width = width;
       this.height = height;
       this.particles = [];
-      this.noParticles = 100;
+      this.noParticles = 1000;
+      this.cellSize = 10;
+      this.cols = 0;
+      this.rows = 0;
+      this.flowField = [];
+      this.curve = 2.5;
+      this.zoom = 0.1;
       this.init();
     }
     init() {
+      // Create flow field
+      this.cols = Math.floor(this.width / this.cellSize);
+      this.rows = Math.floor(this.height / this.cellSize);
+      this.flowField = [];
+      for (let posY = 0; posY < this.rows; posY++) {
+        for (let posX = 0; posX < this.cols; posX++) {
+          let angle =
+            (Math.cos(posX * this.zoom) + Math.sin(posY * this.zoom)) *
+            this.curve;
+          this.flowField.push(angle);
+        }
+      }
+      // Create particles
       for (let i = 0; i < this.noParticles; i++) {
         this.particles.push(new Particle(this));
       }
